b67e66890e9eb96ee12b1325f54cb9450cfdf191
Hoshino Tented <hoshinotented@qq.com>
2025-11-24 09:37:28 +0000

    nix: fix rust-src

--- flake.nix
+++ flake.nix
@@ -11,13 +11,17 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
         libclang = pkgs.llvmPackages_18.libclang;
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src" "rust-analyzer"
+          ];
+        };
       in
       {
         devShells.default = with pkgs; mkShell {
           buildInputs = [
             # rust-overlay
-            openssl pkg-config eza fd 
-            rust-bin.stable.latest.default
+            openssl pkg-config eza fd rust
 
             # libclang
             libclang
